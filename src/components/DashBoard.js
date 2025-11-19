0e88e5a240fcfce96f873f12a2797389986fa519
rohitszz <rohitchoudhary0212u@gmail.com>
2025-11-19 12:05:48 +0530

    Implement logout button in DashBoard component
    Added logout functionality with a button that triggers an API call to log the user out.

--- src/components/DashBoard.js
+++ src/components/DashBoard.js
@@ -1,10 +1,26 @@
 import React from 'react'
-
+import { Navigate, useNavigate } from 'react-router-dom'
 const DashBoard = ({}) => {
+  const navigate = useNavigate();
+  async function changeHandler() {
+    try{
+      const res = await fetch('http://localhost:5000/api/users/dashboard/logout', {
+        method: "POST",
+        credentials: "include"
+      })
+      navigate("/");
+    }
+    catch(error){
+
+    }
+  }
   return (
 
-    <div>
-      This is DashBoard
+    <div className=''>
+
+    <button onClick={changeHandler}>logout</button>
+    <div>This is DashBoard</div>
+      
     </div>
   )
 }
