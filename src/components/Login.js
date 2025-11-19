d3377314d799e7caf1109e5cd9042b9250f147b0
rohitszz <rohitchoudhary0212u@gmail.com>
2025-11-19 12:07:56 +0530

    Update fetch request to include credentials
    Added 'credentials: "include"' to fetch options for login.

--- src/components/Login.js
+++ src/components/Login.js
@@ -20,6 +20,7 @@ const Login = ({isLoggedIn, setIsLoggedIn}) => {
    const res = await fetch("http://localhost:5000/api/users/login", {
       method: "POST",
       headers: { "Content-Type": "application/json"},
+      credentials: "include", 
       body: JSON.stringify({
         email, password,
       })
