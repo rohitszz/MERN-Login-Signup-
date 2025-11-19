ac559cb3af4853deb2535040760113c89bb124b3
rohitszz <rohitchoudhary0212u@gmail.com>
2025-11-19 12:04:24 +0530

    Add authentication routes for auth check and logout

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,11 +3,14 @@ const User = require('../models/user');
 
 const {login} = require('../controllers/login');
 const {signupToken, sendOtp, verifyOtp} = require('../controllers/signup'); 
+const {authcheck, logout} = require("../controllers/auth");
 
 router.post('/login', login);
 router.post('/signuptoken', signupToken);
 router.post('/signup/sendotp', sendOtp);
 router.post('/signup/verifyotp', verifyOtp);
+router.get('/authVerify', authcheck);
+router.post('/dashboard/logout', logout);
 
 module.exports = router;
   
