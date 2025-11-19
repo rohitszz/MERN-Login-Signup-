3d939308f7bf9278a0967aabe897efd7a47c5ebf
rohitszz <rohitchoudhary0212u@gmail.com>
2025-11-19 12:04:53 +0530

    Add new origin to CORS configuration

--- index.js
+++ index.js
@@ -15,10 +15,11 @@ connect();
 
 app.use(cors(
     {
-        origin: ['http://localhost:3001', 'http://localhost:3000'],
+        origin: ['http://localhost:3001', 'http://localhost:3000',  "http://localhost:5173",],
         credentials: true,
     }
 ))
+
 
 const userRoutes = require('./routes/userRoutes');
 
