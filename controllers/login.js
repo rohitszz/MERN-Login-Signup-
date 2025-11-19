8ebae683208076b942b8141b83beb2050cf47af2
rohitszz <rohitchoudhary0212u@gmail.com>
2025-11-19 12:02:57 +0530

    Implement JWT authentication in login controller
    Added JWT token generation and cookie setting for user login.

--- controllers/login.js
+++ controllers/login.js
@@ -1,5 +1,7 @@
 const User = require('../models/user');
 const bcrypt = require('bcrypt');
+const jwt = require("jsonwebtoken");
+
 
 exports.login = async (req, res) => {
     try {
@@ -7,12 +9,27 @@ exports.login = async (req, res) => {
         
         const user = await User.findOne({ email: email });
         
+        
         if(user && await bcrypt.compare( password ,user.password)){
+            const loginToken = jwt.sign(
+                        { email: email},
+                        process.env.JWT_SECRET,
+                        { expiresIn: "7d" }
+                    )
+            
+                    res.cookie("loginAuthToken", loginToken, {
+                        httpOnly: true,
+                        secure: false,
+                        sameSite: "lax",
+                        path:"/", 
+                        maxAge: 7 * 24 * 60 * 60 * 1000
+                    })
+                    
             return res.status(200).json({
                 success: true,
                 message: "User Logged in successfully",
                 userDetails: user
-            })
+            }) 
         }
         else {
             return res.status(401).json({
